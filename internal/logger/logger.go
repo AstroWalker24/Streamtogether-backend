e988693a2b3356a272731dcd34c7d513f49d99aa
AstroWalker24 <walkerastro41@gmail.com>
2026-07-16 21:45:08 +0530

    add slog logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,1 +1,35 @@
 package logger
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/AstroWalker24/Streamtogether-backend/internal/config"
+)
+
+func New(cfg config.LoggingConfig) *slog.Logger {
+	opts := &slog.HandlerOptions{Level: parseLevel(cfg.Level)}
+
+	var handler slog.Handler
+
+	if cfg.Format == "json" {
+		handler = slog.NewJSONHandler(os.Stdout, opts)
+	} else {
+		handler = slog.NewTextHandler(os.Stdout, opts)
+	}
+
+	return slog.New(handler)
+}
+
+func parseLevel(level string) slog.Level {
+	switch level {
+	case "debug":
+		return slog.LevelDebug
+	case "warn":
+		return slog.LevelWarn
+	case "error":
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
+	}
+}
