963c703cf20d585d685bd41185448c8724212da4
AstroWalker24 <walkerastro41@gmail.com>
2026-07-17 08:36:33 +0530

    add server related functions

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,38 @@
 package server
+
+import (
+	"net/http"
+	"context"
+	"github.com/AstroWalker24/Streamtogether-backend/internal/config"
+)
+
+
+type Server struct {
+	http *http.Server
+}
+
+func New(cfg config.ServerConfig, appCfg config.AppConfig, handler http.Handler) *Server {
+	return &Server{
+		http: &http.Server{
+			Addr: appCfg.Address(),
+			Handler: handler,
+			ReadTimeout: cfg.ReadTimeout,
+			WriteTimeout: cfg.WriteTimeout,
+			IdleTimeout: cfg.IdleTimeout,
+		},
+	}
+}
+
+func (s *Server) Start() error {
+	err := s.http.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		return err
+	}
+	return nil
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.http.Shutdown(ctx)
+} 
+
+
